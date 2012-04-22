56c659d5c7ba713e1fce1c67b8473307b1ff473a
Javed Khan <javed@agiliq.com>
2012-04-22 16:39:49 +0530

    compatible with heroku redis server

--- reloadr/watch.py
+++ reloadr/watch.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 import os
-
 import redis
+import urlparse
 import pyinotify
 
 from juggernaut import Juggernaut
@@ -20,13 +20,19 @@ EXTENSIONS = [
 
 REDIS_SERVER = "localhost"
 
+if os.environ.has_key('REDISTOGO_URL'):
+    urlparse.uses_netloc.append('redis')
+    url = urlparse.urlparse(os.environ['REDISTOGO_URL'])
+    REDIS = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+else:
+    REDIS = redis.Redis(REDIS_SERVER)
+
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
         if any([True for ext in EXTENSIONS if event.pathname.endswith(ext)]):
             msg = "Modified: %s" % (event.pathname)
             print msg
-            r = redis.Redis(REDIS_SERVER)
-            jug = Juggernaut(r)
+            jug = Juggernaut(REDIS)
             jug.publish("channel1", msg)
 
 handler = EventHandler()
